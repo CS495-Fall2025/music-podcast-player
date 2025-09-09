b51288d452b18f121a347ed5a09ba6bf3ae17867
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-09-08 22:37:54 -0400

    Add structure for local rss feed and player to main page (#80)

---
+++ frontend/src/components/LocalFeed.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TrackFeedEntry from "./TrackFeedEntry.vue";
+</script>
+
+<template>
+  <div>
+    <p>Create a feed of tracks here</p>
+    <TrackFeedEntry />
+  </div>
+</template>
+
+<style scoped></style>
