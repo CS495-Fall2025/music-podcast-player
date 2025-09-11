b440377b3df3fb949337f23c539f10b9e9036a2f
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-09-11 19:19:13 -0400

    Link MiniPlayer to currentTrack and remove LocalFeed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 // Import components here.
-import LocalFeed from "./components/LocalFeed.vue";
 import NavigationBar from "./components/NavigationBar.vue";
 import MiniPlayer from "./components/MiniPlayer.vue";
 import RSSFeedForm from "./components/RSSFeedForm.vue";
@@ -10,7 +9,6 @@ import UserFeed from "./components/UserFeed.vue";
 <template>
   <NavigationBar />
   <RSSFeedForm />
-  <LocalFeed />
   <MiniPlayer />
   <UserFeed />
 </template>
