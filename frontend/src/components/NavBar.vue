4d7f027c8f322baea54340a45899cf4e0e3de9f9
Evan Greenfield <112117255+egreenfield323@users.noreply.github.com>
2025-10-07 16:00:22 -0400

    Create a navigation menu bar
    * Create simple navbar
    
    * add bootstrap-vue-next
    
    bootstrap-vue-next is the Vue3 version of Vue2 bootstrap components
    
    * fix format
    
    * update simple nav bar
    
    * update navbar styling
    
    * run linter
    
    * remove NavigationBar from App.Vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <BNavbar class="navbar" toggleable="lg">
+    <BNavbarBrand href="#" class="brand">RSS Music Player</BNavbarBrand>
+
+    <BNavbarToggle target="nav-collapse" />
+
+    <BCollapse id="nav-collapse" is-nav>
+      <BNavbarNav>
+        <BNavItem href="#" class="nav-link">Home</BNavItem>
+        <BNavItem href="#" class="nav-link">About</BNavItem>
+        <BNavItem href="#" class="nav-link">Contact</BNavItem>
+      </BNavbarNav>
+
+      <BNavbarNav class="ms-auto">
+        <BNavItemDropdown text="Account" class="nav-link">
+          <BDropdownItem @click="onLogin">Login</BDropdownItem>
+          <BDropdownItem @click="onLogout">Logout</BDropdownItem>
+        </BNavItemDropdown>
+      </BNavbarNav>
+    </BCollapse>
+  </BNavbar>
+</template>
+
+<script setup>
+import {
+  BNavbar,
+  BNavbarBrand,
+  BNavbarToggle,
+  BCollapse,
+  BNavbarNav,
+  BNavItem,
+  BNavItemDropdown,
+  BDropdownItem,
+} from "bootstrap-vue-next";
+
+import navbarLogic from "./navBar.js";
+const { onLogin, onLogout } = navbarLogic;
+</script>
+
+<style src="../style.css" />
