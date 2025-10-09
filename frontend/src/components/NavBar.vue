83939a6ad0c8f184a456c728a94aede889350b7e
Evan Greenfield <evangreenfield@live.com>
2025-10-08 21:12:29 -0400

    run linter

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,7 +3,11 @@
     <div class="nav-left">
       <div class="brand">RSS Music Player</div>
 
-      <button class="navbar-toggle" @click="isOpen = !isOpen" aria-label="Toggle navigation" />
+      <button
+        class="navbar-toggle"
+        @click="isOpen = !isOpen"
+        aria-label="Toggle navigation"
+      />
 
       <div :class="['nav-links', { 'is-open': isOpen }]">
         <a href="#" class="nav-link">Home</a>
@@ -14,7 +18,10 @@
 
     <div class="nav-right">
       <div class="nav-dropdown" ref="dropdownRef">
-        <button class="dropdown-toggle" @click.stop="dropdownOpen = !dropdownOpen">
+        <button
+          class="dropdown-toggle"
+          @click.stop="dropdownOpen = !dropdownOpen"
+        >
           Account ▾
         </button>
         <div v-if="dropdownOpen" class="dropdown-menu">
