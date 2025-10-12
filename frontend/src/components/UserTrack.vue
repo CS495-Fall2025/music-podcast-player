2b233344a6b63e9899ed374d457c7086cac6393d
Spencer Smith <112116446+spencersmith24@users.noreply.github.com>
2025-10-11 20:10:46 -0400

    41 implement light and dark mode (#98)
    * reorganize style sheet
    
    * move specific styling from universal sheet
    
    take component specific styling and move it to each components vue file
    
    * implement light/dark colors for miniplayer
    
    * Remove excess styling
    
    * remove comment
    
    * remove MORE excess styling
    
    * run linter
    
    * run linter again

--- frontend/src/components/UserTrack.vue
+++ frontend/src/components/UserTrack.vue
@@ -8,3 +8,44 @@
     <div class="track-title">{{ track.title }}</div>
   </div>
 </template>
+
+<style scoped>
+.track {
+  cursor: pointer;
+  text-align: center;
+  background-color: var(--track-background);
+  margin: 5px;
+  box-shadow: var(--shadow-md);
+  border-radius: var(--border-radius-sm);
+  transition:
+    transform 0.2s ease,
+    background-color 0.2s ease,
+    color 0.2s ease;
+}
+
+.track:hover {
+  background-color: var(--hover-blue);
+  color: var(--dark-blue);
+  transform: scale(1.05);
+  z-index: var(--track-hover-z);
+}
+
+.track-image {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: var(--border-radius);
+  margin-top: 10px;
+}
+
+.track-title {
+  font-size: 16px;
+  font-weight: bold;
+  margin: 1px;
+  inline-size: 125px;
+  overflow-wrap: break-word;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
