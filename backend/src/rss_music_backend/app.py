d69413fd8096c35b31d6d133bd90ed50af052e9e
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-11-13 10:46:40 -0500

    Add lambda interface to backend

--- backend/src/rss_music_backend/app.py
+++ backend/src/rss_music_backend/app.py
@@ -1,5 +1,6 @@
 import os
 
+from apig_wsgi import make_lambda_handler
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
@@ -38,3 +39,6 @@ def apply_blueprints(app: Flask) -> None:
 
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
+
+
+lambda_handler = make_lambda_handler(create_app())
