2ba36984e78876169487e683bffc85fd071a435f
Brody Roth <brodyroth23@gmail.com>
2025-11-22 21:21:19 -0500

    Adding RSS Parser functions to backend. Includes endpoint designation and functions for retrieving RSS Data from an input url.
    This extends our feed parsers functionailty, and items parsed. Includes metadata parsing for entire feed, and individual track.
    
    Still needs frontend parser changes to display the data retrieved.

--- backend/src/rss_music_backend/schemas/__init__.py
+++ backend/src/rss_music_backend/schemas/__init__.py
@@ -6,8 +6,13 @@ from rss_music_backend.schemas.responses.search_feeds_response import (
     SearchFeedsResponseSchema,
 )
 
+from rss_music_backend.schemas.requests.link_feed_request import (
+    LinkFeedRequestSchema,
+)
+
 __all__ = [
     "SAnd",
     "SearchFeedsResponseSchema",
     "SearchFeedsRequestSchema",
+    "LinkFeedRequestSchema",
 ]
