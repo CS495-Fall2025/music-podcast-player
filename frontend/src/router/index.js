f5261ded4ae339f18cf5216b92aebd180b729213
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-12-07 05:27:16 -0500

    Removed BoostTestPage, can test with actual feature using env vars

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,16 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import InputFeedPage from "../pages/InputFeedPage.vue";
 import SearchFeedPage from "../pages/SearchFeedPage.vue";
 import ViewFeedPage from "../pages/ViewFeedPage.vue";
-import BoostTestPage from "../pages/BoostTestPage.vue";
 
 const routes = [
   { path: "/", component: InputFeedPage },
   { path: "/search", component: SearchFeedPage },
   { path: "/view", component: ViewFeedPage },
-  // Development tool pages
+  // Development tool pages, add paths to the else condition.
   ...(import.meta.env.VITE_INCLUDE_DEV_FEATURES !== "yes"
     ? []
-    : [{ path: "/boost", component: BoostTestPage }]),
+    : []),
 ];
 
 const router = createRouter({
