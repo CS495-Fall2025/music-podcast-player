91a00a63c24cb713ba35871b625e637742753c49
Spencer <spencersmith24@outlook.com>
2026-01-08 15:17:55 -0500

    begin track component remodel
    Changes tracks to show the track image on the very left. Then a stack with the title above the artist, then the description. Shortened text is also un-truncated when the track is hovered over.

--- frontend/src/components/UserTrack.vue
+++ frontend/src/components/UserTrack.vue
@@ -4,11 +4,12 @@
 
 <template>
   <div class="track" @click="selectTrack">
+    <img class="track-image" :src="track.image" :alt="track.title" />
     <div class="track-info">
-      <img class="track-image" :src="track.image" :alt="track.title" />
       <div class="track-title">{{ track.title }}</div>
+      <div class="track-artist">Artist with a super long name that goes past the breakpoint for testing reasons</div>
     </div>
-    <div class="track-description">Track description</div>
+    <div class="track-description">Track description that describes the track by describing information in a descriptive way. This text will often contain a description of the track, but can also contain the transcript. The description may also contain html tags that will be parsed by our app in the future to prevent xss and to allow things like links to be used.</div>
   </div>
 </template>
 
@@ -37,31 +38,71 @@
 .track-info {
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: left;
+  align-self: center;
   max-width: 200px;
-  margin: 10px;
-}
-
-.track-image {
-  width: 100px;
-  height: 100px;
-  margin-top: 10px;
-  border-radius: var(--border-radius);
-  object-fit: cover;
+  margin: 5px;
 }
 
 .track-title {
-  inline-size: 125px;
-  margin: 1px;
+  writing-mode: horizontal-tb;
+  min-inline-size: 125px;
+  max-inline-size: 300px;
   font-size: 16px;
   font-weight: bold;
-  text-align: center;
-  overflow-wrap: break-word;
+  text-align: left; 
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+.track:hover .track-title{
+  text-overflow: clip;
+  overflow: visible;
+  white-space: normal;
+}
+
+.track-artist{
+  writing-mode: horizontal-tb;
+  max-inline-size:400px;
+  min-inline-size: 200px;
+  font-size: 12px;
+  text-align: left;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+.track:hover .track-artist{
+  text-overflow: clip;
+  overflow: visible;
+  white-space: normal;
+}
+
+.track-image {
+  width: 50px;
+  height: 50px;
+  margin: 5px;
+  border-radius: var(--border-radius);
+  object-fit: cover;
+  align-self: center;
 }
 
 .track-description {
   width: 100%;
-  margin: 10px;
-  font-size: 16px;
+  margin: 5px;
+  font-size: 14px;
+  overflow: hidden;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+}
+
+.track:hover .track-description{
+  text-overflow: clip;
+  overflow: visible;
+  white-space: normal;
+  -webkit-line-clamp: unset;
 }
 </style>
