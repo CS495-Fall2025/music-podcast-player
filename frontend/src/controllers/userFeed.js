86ddbe6b027b4295594af70dde4017fcd73aad3b
Spencer <spencersmith24@outlook.com>
2026-01-12 11:20:57 -0500

    TYPO

--- frontend/src/controllers/userFeed.js
+++ frontend/src/controllers/userFeed.js
@@ -13,7 +13,7 @@ export default {
       return this.feed.image?.trim() || '/src/assets/images/default-image.jpg'
     },
     feedTitle() { // not yet working, i think it's a parsing thing, will likely need to change some calls
-      return this.feed.title?.trim() || 'Unititled Feed'
+      return this.feed.title?.trim() || 'Untitled Feed'
     },
     feedArtist() { // not yet working, i think it's a parsing thing, will likely need to change some calls
       return this.feed.artist?.trim() || 'Feed artist not found'
