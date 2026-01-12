78e2bff661ccdb2873ab50bd4e95ce16ae36a10a
Spencer <spencersmith24@outlook.com>
2026-01-12 09:15:38 -0500

    begin feed info remodel
    This commit adds a space for the feed's cover image as well as its name at the top of the feed display page. This makes the app feel more like a real audio player by providing more information about what is being listened to.

--- frontend/src/components/UserFeed.vue
+++ frontend/src/components/UserFeed.vue
@@ -3,6 +3,11 @@
 <script src="../controllers/userFeed.js"></script>
 
 <template>
+  <div class="feed-info">
+    <img class="feed-image" :src="feed.image" :alt="feed.title" />
+    <div class="feed-title"> Feed Title</div>
+
+  </div>
   <div class="user-feed">
     <Track v-for="(item, index) in feed" :key="index" :track="item" />
   </div>
@@ -20,5 +25,25 @@
   overflow-x: hidden;
   padding-bottom: 10px;
   padding-top: 10px;
+}
+
+.feed-info{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.feed-image{
+  margin: 20px 0px 10px 0px;
+  height: 200px;
+  width: 200px;
+}
+
+.feed-title{
+  font-size: 36px;
+  font-weight: bold;
+  inline-size: auto;
+  max-width: 90%;
+  text-align: center;
 }
 </style>
