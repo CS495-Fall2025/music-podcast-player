5c63512ba047e885b4151449e0dd03d32170d6f4
Spencer Smith <112116446+spencersmith24@users.noreply.github.com>
2026-01-13 10:30:19 -0500

    49 new list display (#122)
    * begin track component remodel
    
    Changes tracks to show the track image on the very left. Then a stack with the title above the artist, then the description. Shortened text is also un-truncated when the track is hovered over.
    
    * begin feed info remodel
    
    This commit adds a space for the feed's cover image as well as its name at the top of the feed display page. This makes the app feel more like a real audio player by providing more information about what is being listened to.
    
    * add default 'not found' image
    
    * use variables from scripts
    
    Changes things like track.image to use a variable defined in the corresponding script
    
    * improve styling syntax
    
    * add comments
    
    * further improve styling syntax for userfeed
    
    * set track-info size based on content length
    
    * change track image sizing
    
    * TYPO
    
    * run format fix

--- frontend/src/controllers/userFeed.js
+++ frontend/src/controllers/userFeed.js
@@ -9,5 +9,17 @@ export default {
     feed() {
       return feed;
     },
+    feedImage() {
+      // not yet working, i think it's a parsing thing, will likely need to change some calls
+      return this.feed.image?.trim() || "/src/assets/images/default-image.jpg";
+    },
+    feedTitle() {
+      // not yet working, i think it's a parsing thing, will likely need to change some calls
+      return this.feed.title?.trim() || "Untitled Feed";
+    },
+    feedArtist() {
+      // not yet working, i think it's a parsing thing, will likely need to change some calls
+      return this.feed.artist?.trim() || "Feed artist not found";
+    },
   },
 };
