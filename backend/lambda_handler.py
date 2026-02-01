0106bf7f8d60c3877ff9d26686179b0e8484c79c
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2026-02-01 16:43:14 -0500

    Give backend access to Database instance

--- backend/lambda_handler.py
+++ backend/lambda_handler.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import awsgi
@@ -11,18 +12,34 @@ from rss_music_backend import create_app
 
 def populate_secrets() -> None:
     ssm_client = boto3.client("ssm")
+    secrets_client = boto3.client("secretsmanager")
 
-    var_to_routes = {
+    ssm_var_to_routes = {
         "SSM_ROUTE_PODCAST_INDEX_KEY": "RSS_PLAYER_PODCAST_INDEX_KEY",
         "SSM_ROUTE_PODCAST_INDEX_SECRET": "RSS_PLAYER_PODCAST_INDEX_SECRET",
-        "SSM_ROUTE_DATABASE_CONNECTION": "RSS_PLAYER_DATABASE_CONNECTION",
     }
 
-    for route_var, env_var in var_to_routes.items():
+    for route_var, env_var in ssm_var_to_routes.items():
         route = os.environ[route_var]
         response = ssm_client.get_parameter(Name=route, WithDecryption=True)
         value = response["Parameter"]["Value"]
         os.environ[env_var] = value
+
+    # Populate database connection string.
+    connection_url = os.environ["DATABASE_CONNECTION_PARTIAL"]
+
+    db_credentials = json.loads(
+        secrets_client.get_secret_value(
+            SecretId=os.environ["DATABASE_SECRET_ARN"]
+        )["SecretString"]
+    )
+
+    connection_url = connection_url.format(
+        user=db_credentials["username"],
+        password=db_credentials["password"]
+    )
+
+    os.environ["RSS_PLAYER_DATABASE_CONNECTION"] = connection_url
 
 
 populate_secrets()
--- cdk/cdk/stack.py
+++ cdk/cdk/stack.py
@@ -9,7 +9,9 @@ from aws_cdk import (
     aws_apigateway as apigw,
     aws_cloudfront as cloudfront,
     aws_cloudfront_origins as origins,
+    aws_ec2 as ec2,
     aws_lambda as _lambda,
+    aws_rds as rds,
     aws_ssm as ssm,
     aws_s3 as s3,
     aws_s3_deployment as s3_deploy
@@ -25,11 +27,14 @@ FRONTEND_BUILD = FRONTEND_PATH / "dist"
 class RSSMusicPlayerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        database = self._make_database()
         
         frontend_bucket = self._make_frontend_bucket()
         frontend_distribution = self._make_frontend_distribution(frontend_bucket)
 
         backend_function = self._make_backend_function(
+            database,
             frontend_distribution.domain_name
         )
 
@@ -106,7 +111,7 @@ class RSSMusicPlayerStack(Stack):
 
         return distribution
 
-    def _make_backend_function(self, frontend_domain) -> _lambda.Function:
+    def _make_backend_function(self, database, frontend_domain) -> _lambda.Function:
         # These are REFERENCES to keys that MUST be created manually. AWS doesn't
         # support creating SecureString parameters through the CDK, and we'd need to set
         # the values manually anyways.
@@ -121,11 +126,6 @@ class RSSMusicPlayerStack(Stack):
                 "RSSMusicPlayerPodcastIndexAPISecret",
                 parameter_name="/rss-music-player/podcast-index-api/secret",
             ),
-            ssm.StringParameter.from_secure_string_parameter_attributes(
-                self,
-                "RSSMusicPlayerDatabaseConnectionURL",
-                parameter_name="/rss-music-player/database/connection-url",
-            ),
         ]
 
         function = _lambda.Function(
@@ -138,17 +138,23 @@ class RSSMusicPlayerStack(Stack):
             architecture=_lambda.Architecture.ARM_64,
             environment={
                 "RSS_PLAYER_ALLOWED_ORIGINS": f"https://{frontend_domain}",
-                "SSM_ROUTE_PODCAST_INDEX_KEY":
+                "PODCAST_INDEX_KEY_ARN":
                     "/rss-music-player/podcast-index-api/key",
-                "SSM_ROUTE_PODCAST_INDEX_SECRET":
+                "PODCAST_INDEX_SECRET_ARN":
                     "/rss-music-player/podcast-index-api/secret",
-                "SSM_ROUTE_DATABASE_CONNECTION":
-                    "/rss-music-player/database/connection-url",
+                "DATABASE_CONNECTION_PARTIAL":
+                    "postgresql://{user}:{password}@"
+                    f"{database.db_instance_endpoint_address}:"
+                    f"{database.db_instance_endpoint_port}",
+                "DATABASE_SECRET_ARN":
+                    database.secret.secret_arn,
             },
         )
 
         for secret in backend_secrets:
             secret.grant_read(function)
+
+        database.secret.grant_read(function)
 
         return function
 
@@ -189,3 +195,49 @@ class RSSMusicPlayerStack(Stack):
         )
 
         return deployment
+
+    def _make_database(self) -> rds.DatabaseInstance:
+        #vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
+        vpc = ec2.Vpc(self, "RSSMusicPlayerDatabaseVpc", max_azs=3)
+        security_group = ec2.SecurityGroup.from_security_group_id(
+            self, "DefaultSG", vpc.vpc_default_security_group
+        )
+
+
+        database = rds.DatabaseInstance(
+            self,
+            "RSSMusicPlayerDatabase",
+            engine=rds.DatabaseInstanceEngine.postgres(
+                version=rds.PostgresEngineVersion.VER_17
+            ),
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MICRO,
+            ),
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+            ),
+            security_groups=[security_group],
+            multi_az=False,
+            allocated_storage=20,
+            publicly_accessible=False,
+            storage_encrypted=True,
+            credentials=rds.Credentials.from_generated_secret(username="rssmusicplayer"),
+            backup_retention=Duration.days(1),
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        database.connections.allow_default_port_from_any_ipv4(
+            description="Necessary to allow backend to connect"
+        )
+
+        CfnOutput(
+            self,
+            "RSSMusicPlayerDatabaseEndpoint",
+            value=(
+                f"{database.db_instance_endpoint_address}:"
+                f"{database.db_instance_endpoint_port}"
+            )
+        )
+
+        return database
