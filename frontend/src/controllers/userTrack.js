841227620d96440ec5de7686e8dd4beb05ab388e
Spencer <spencersmith24@outlook.com>
2026-02-06 10:19:40 -0500

    add comments

--- frontend/src/controllers/userTrack.js
+++ frontend/src/controllers/userTrack.js
@@ -11,12 +11,14 @@ export default {
   },
 
   methods: {
+    // Sets the currentTrack variable to this track.
     selectTrack() {
       currentTrack.value = this.track;
     },
   },
 
   computed: {
+    // Returns a trackObj if the track exists, otherwise return placeholder values to prevent crashing.
     trackObj() {
       return this.track || {
         image: "",
@@ -25,15 +27,19 @@ export default {
         description: ""
       }
     },
+    // Returns the track's image, if there's no image, returns placeholder.
     trackImage() {
       return this.trackObj.image || "/src/assets/images/default-image.jpg";
     },
+    // Returns the track's artist, if there's no artist, returns placeholder.
     trackArtist() {
       return this.trackObj.artist || "Track artist not found";
     },
+    // Returns the track's title, if there's no title, returns placeholder.
     trackTitle() {
       return this.trackObj.title || "Track title not found";
     },
+    // Returns the track's description, if there's no description, returns placeholder.
     trackDescription() {
       return this.trackObj.description || "Track description not found";
     },
