7e48eda8de72d0e9e2b53161a3c8f9fc27da70ae
Spencer <spencersmith24@outlook.com>
2026-02-27 09:25:59 -0500

    import playlist

--- backend/packages/data_model/src/rss_music_data_model/__init__.py
+++ backend/packages/data_model/src/rss_music_data_model/__init__.py
@@ -1,6 +1,7 @@
 from rss_music_data_model.base import Base
 from rss_music_data_model.engine import get_engine, initialize_engine, make_session
 from rss_music_data_model.users import User
+from rss_music_data_model.playlist import Playlist
 
 
 __all__ = [
