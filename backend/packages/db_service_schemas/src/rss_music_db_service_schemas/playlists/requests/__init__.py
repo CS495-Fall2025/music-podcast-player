cc06a882a605fe5b656fd81275df61b7fdff8fdf
Spencer <spencersmith24@outlook.com>
2026-03-12 22:11:57 -0400

    create playlist request and response init files
    Issue 4 of Evan's review

---
+++ backend/packages/db_service_schemas/src/rss_music_db_service_schemas/playlists/requests/__init__.py
@@ -0,0 +1,9 @@
+from rss_music_db_service_schemas.playlists.requests.create_playlist import CreatePlaylistRequest
+from rss_music_db_service_schemas.playlists.requests.update_playlist import UpdatePlaylistRequest
+from rss_music_db_service_schemas.playlists.requests.delete_playlist import DeletePlaylistRequest
+
+__all__ = [
+    "CreatePlaylistRequest",
+    "UpdatePlaylistRequest",
+    "DeletePlaylistRequest",
+]
