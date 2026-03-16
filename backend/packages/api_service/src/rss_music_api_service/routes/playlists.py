cee5c138e748998f7cbf4cb6de6dba3bc478b152
Spencer <spencersmith24@outlook.com>
2026-03-16 09:55:03 -0400

    Handle errors when updating/deleting unknown playlist

--- backend/packages/api_service/src/rss_music_api_service/routes/playlists.py
+++ backend/packages/api_service/src/rss_music_api_service/routes/playlists.py
@@ -13,8 +13,8 @@ PLAYLISTS_BP = Blueprint("playlists", __name__, url_prefix="/playlists")
 @login_required
 def create_playlist():
     user_id = get_current_user_id()
-
     payload = request.get_json(silent=True) or {}
+
     payload["created_by_user_id"] = int(user_id)
     request_data = playlist_reqs.CreatePlaylistRequest().load(payload)
 
@@ -44,19 +44,23 @@ def get_user_playlists():
 def update_playlist(id):
     # Load the update data (title/description)
     user_id = get_current_user_id()
-
     payload = request.get_json(silent=True) or {}
     payload["created_by_user_id"] = int(user_id)
     request_data = playlist_reqs.UpdatePlaylistRequest().load(payload)
 
     # Pass the ID from the URL and the user_id for security
-    result = db_service.update_playlist(
+    updated_playlist = db_service.update_playlist(
         playlist_id=id,
         user_id=user_id,
         title=request_data.get("title"),
         description=request_data.get("description"),
     )
-    return result, 200
+
+    if updated_playlist is None:
+        return {"error": "Not Found",
+                "message": "Playlist not found or unauthorized"
+                }, 404
+    return playlist_reqs.UpdatePlaylistResponse().dump(updated_playlist), 200
 
 
 # Delete playlist
@@ -65,10 +69,13 @@ def update_playlist(id):
 @PLAYLISTS_BP.delete("/<int:id>")
 @login_required
 def delete_playlist(id):
-    user_id = get_current_user_id()
+    user_id = int(get_current_user_id())
 
-    user_id = int(user_id)
+    success = db_service.delete_playlist(playlist_id=id, user_id=user_id)
 
-    db_service.delete_playlist(playlist_id=id, user_id=user_id)
+    if not success:
+        return {"error": "Not Found",
+                "message": "Playlist not found or unauthorized"
+                }, 404
 
     return {"message": "Playlist deleted successfully"}, 200
