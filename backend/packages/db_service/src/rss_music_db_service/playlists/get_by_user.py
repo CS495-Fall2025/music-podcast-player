d88f793b4564799e503cd156373707531c518737
Spencer <spencersmith24@outlook.com>
2026-03-16 09:17:36 -0400

    add user check to get_by_user

--- backend/packages/db_service/src/rss_music_db_service/playlists/get_by_user.py
+++ backend/packages/db_service/src/rss_music_db_service/playlists/get_by_user.py
@@ -1,6 +1,12 @@
-from rss_music_data_model import Playlist, make_session
+from backend.packages.db_service.src.rss_music_db_service.errors import UserNotFoundError
+from rss_music_data_model import Playlist, User, make_session
 
 
 def get_playlists_by_user(user_id: int) -> list[Playlist]:
     with make_session() as session:
+        user = session.query(User).filter_by(id=user_id).first()
+
+        if not user:
+            raise UserNotFoundError(f"User with ID {user_id} does not exist.")
+
         return session.query(Playlist).filter_by(created_by_user_id=user_id).all()
