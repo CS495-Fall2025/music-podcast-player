e7feeb395657154c4616a69275bd4dcda68630e1
Spencer <spencersmith24@outlook.com>
2026-03-16 09:13:57 -0400

    Implemenet UserNotFoundError

--- backend/packages/db_service/src/rss_music_db_service/errors.py
+++ backend/packages/db_service/src/rss_music_db_service/errors.py
@@ -2,3 +2,8 @@ class NotUniqueError(ValueError):
     def __init__(self, field: str):
         super().__init__(f"{field} was not unique")
         self.field = field
+
+
+class UserNotFoundError(Exception):
+    """Raised when the provided user_id does not exist in the database"""
+    pass
--- backend/packages/db_service/src/rss_music_db_service/playlists/create.py
+++ backend/packages/db_service/src/rss_music_db_service/playlists/create.py
@@ -1,3 +1,4 @@
+from backend.packages.db_service.src.rss_music_db_service.errors import UserNotFoundError
 from sqlalchemy.exc import IntegrityError
 from rss_music_data_model import Playlist, make_session
 from rss_music_db_service.logging_config import log_request, get_logger
@@ -31,4 +32,7 @@ def create_and_add_playlist(
             return playlist
         except IntegrityError as error:
             session.rollback()
-            raise error
+
+            logger.error(f"Failed to create playlist: {error}")
+            raise UserNotFoundError(
+                f"User ID {user_id} does not exist.") from error
