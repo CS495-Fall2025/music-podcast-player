a657dffdd0485f8ef09c6b1a54b0cd96ed34e3da
Spencer <spencersmith24@outlook.com>
2026-03-30 11:14:48 -0400

    remove commented code

--- frontend/src/pages/ViewFeedPage.vue
+++ frontend/src/pages/ViewFeedPage.vue
@@ -15,19 +15,9 @@ import UserFeed from "../components/UserFeed.vue";
   display: flex;
   flex-direction: column;
   flex: 1;
-
-  /* height: 100dvh; */
   min-height: 0;
   width: 100%;
 
   overflow: hidden;
 }
 </style>
-
-<!-- 
-<template>
-  <UserFeed />
-  <MiniPlayer />
-</template>
-
-<style scoped></style> -->
