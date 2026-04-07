5158884cc8ac07bf6509b9e9caa1885457237c9a
Spencer Smith <112116446+spencersmith24@users.noreply.github.com>
2026-04-07 14:04:17 -0400

    213 track tweaks (#223)
    * change returning "Unknown" to ""
    
    * show track numbers on each track
    
    * display fixed track number
    
    assign a track number to each track at the time of loading the local store. this will prevent the first track from always being 'track 1' when reversed.
    
    * indicate which track is currently active
    
    * add new blue

--- frontend/src/controllers/userTrack.js
+++ frontend/src/controllers/userTrack.js
@@ -30,23 +30,33 @@ export default {
         }
       );
     },
+    // Returns True if this is the currently playing track, false if not.
+    isActive() {
+      return currentTrack.value === this.track;
+    },
     // Returns the track's image, if there's no image, returns placeholder.
     trackImage() {
       return this.trackObj.image || "/src/assets/images/default-image.jpg";
     },
-    // Returns the track's artist, if there's no artist, returns placeholder.
+    // Returns the track's artist, if there's no artist, returns empty.
     trackArtist() {
-      return this.trackObj.artist || "Track artist not found";
+      if (this.trackObj.artist === "Unknown") return "";
+      return this.trackObj.artist;
     },
-    // Returns the track's title, if there's no title, returns placeholder.
+    // Returns the track's title, if there's no title, returns empty.
     trackTitle() {
-      return this.trackObj.title || "Track title not found";
+      if (this.trackObj.title === "Unknown") return "";
+      return this.trackObj.title;
     },
-    // Returns the track's (sanitized) description, if there's no description, returns placeholder.
+    // Returns the track's (sanitized) description, if there's no description, returns empty.
     trackDescription() {
-      return (
-        sanitizeText(this.trackObj.description) || "Track description not found"
-      );
+      if (this.trackObj.description === "Unknown") return "";
+      return sanitizeText(this.trackObj.description);
+    },
+    trackNumber() {
+      if (this.trackObj.trackNumber)
+        return "Track " + this.trackObj.trackNumber;
+      return "";
     },
   },
 };
