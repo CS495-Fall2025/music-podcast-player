2d29c6197b30dc141740f107e3ba3d0e17aac8e8
Spencer Smith <112116446+spencersmith24@users.noreply.github.com>
2026-04-19 12:21:23 -0400

    Volume sync (#247)
    * synchronize volume value across different feeds
    
    * format
    
    * save volume level across feeds
    
    Fixes an issue where the volume slider would save its position between feeds, but the actual volume value would not
    
    * add comments to miniplayer.js

--- frontend/src/components/MiniPlayer.vue
+++ frontend/src/components/MiniPlayer.vue
@@ -3,9 +3,7 @@
 <script setup>
 import BoostModal from "./BoostModal.vue";
 import { useMiniPlayer } from "../controllers/miniplayer.js";
-import { ref } from "vue";
-
-const volume = ref(1);
+import { currentVolume } from "../controllers/localFeedStore.js";
 
 defineProps({
   showReverse: {
@@ -153,8 +151,8 @@ const {
           min="0"
           max="1"
           step="0.01"
-          v-model="volume"
-          @input="audioRef.volume = volume"
+          v-model="currentVolume"
+          @input="audioRef.volume = currentVolume"
         />
       </div>
     </div>
